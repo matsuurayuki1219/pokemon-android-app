b01442822d8f4290b9accce39b24691f4b13f1e9
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-04-29 21:11:00 +0900

    feat: set up timber.

--- app/src/main/java/jp/matsuura/pokemon/androidapp/ui/PokemonApplication.kt
+++ app/src/main/java/jp/matsuura/pokemon/androidapp/ui/PokemonApplication.kt
@@ -2,6 +2,21 @@ package jp.matsuura.pokemon.androidapp.ui
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import jp.matsuura.pokemon.androidapp.BuildConfig
+import timber.log.Timber
 
 @HiltAndroidApp
-class PokemonApplication : Application()
+class PokemonApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        setUpTimber()
+    }
+
+    private fun setUpTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
