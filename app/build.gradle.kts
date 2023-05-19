8ef7f7bb2051b93648b2410a82370a290c05f9b5
matsuurayuki1219 <yuki.matsuura@progrit.co.jp>
2023-05-19 20:00:33 +0900

    feat: introduce version catalog.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,23 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.android.application)
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.kotlin)
+    @Suppress("DSL_SCOPE_VIOLATION")
+    alias(libs.plugins.dagger.hilt)
     id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "jp.matsuura.pokemon.androidapp"
-    compileSdk = 33
+    compileSdk = libs.versions.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "jp.matsuura.pokemon.androidapp"
-        minSdk = 26
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        minSdk = libs.versions.minSdk.get().toInt()
+        targetSdk =  libs.versions.targetSdk.get().toInt()
+        versionCode =  libs.versions.versionCode.get().toInt()
+        versionName =  libs.versions.versionName.get()
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -51,39 +54,36 @@ android {
 dependencies {
 
     // default
-    implementation("androidx.core:core-ktx:1.10.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.1")
-    implementation("androidx.compose.ui:ui:1.4.2")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.4.2")
-    implementation("androidx.compose.material3:material3:1.0.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.4.2")
-    // debugImplementation("androidx.compose.ui:ui-tooling:1.4.3")
-    // debugImplementation("androidx.compose.ui:ui-test-manifest:1.4.2")
-
+    implementation(libs.androidx.coreKtx)
+    implementation(libs.androidx.lifecycle)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.lifecyle.compose)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso)
+    androidTestImplementation(libs.androidx.compose.ui.test.junit)
 
     // navigation compose
-    implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.hilt.navigation.compose)
 
     // dagger hilt
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-compiler:2.44")
+    implementation(libs.dagger.hilt)
+    kapt(libs.dagger.hilt.compiler)
 
     // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.moshi)
 
     // moshi
-    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation(libs.moshi)
 
     // timber
-    implementation("com.jakewharton.timber:timber:5.0.1")
+    implementation(libs.timber)
 
     // coil
-    implementation("io.coil-kt:coil-compose:2.3.0")
+    implementation(libs.coil)
 }
